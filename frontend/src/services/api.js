12b1cf5d5042f307f93851046143239c2ae577ef
Khalidkhan69 <khalidkhan09009@gmail.com>
2026-07-15 15:59:05 +0500

    Update API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "rms-fyp-production.up.railway.app",
+    baseURL: "https://rms-fyp-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
